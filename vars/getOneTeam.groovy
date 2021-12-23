1ebfa32a39ccd684f055396cfe917fe500d40137
cx-leonelsanches <leonel.sanches@checkmarx.com>
2021-12-22 16:17:29 -0800

    Assertions.

--- vars/getOneTeam.groovy
+++ vars/getOneTeam.groovy
@@ -6,6 +6,8 @@
 import com.checkmarx.jenkinssharedlibrary.RestClient
 
 def call(String bearerToken, int teamId) {
+    assert env.hasProperty('CX_SERVER_URL') && env.CX_SERVER_URL : 'Variable CX_SERVER_URL not set'
+
     RestClient restClient = new RestClient()
     return restClient.get("${env.CX_SERVER_URL}/cxrestapi/auth/teams/${teamId}", bearerToken)
 }
